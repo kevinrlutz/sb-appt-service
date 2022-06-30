b07df24315925d1e30facc45532a4139e6fe872e
kevinrlutz <kevinlutz429@gmail.com>
2022-06-30 13:21:47 -0400

    initial commit

---
+++ src/main/java/com/perficient/apptservice/ApptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.apptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptServiceApplication.class, args);
+	}
+
+}
