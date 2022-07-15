9e935b842360554b4d0cf9a4fa52d557ce016a61
Rayaan Attari <rayaan.attari@gmail.com>
2022-07-14 23:52:00 -0500

    Add Eureka configuration
    - Register microservice with Eureka server
    - Update port number

--- src/main/java/com/perficient/apptservice/ApptServiceApplication.java
+++ src/main/java/com/perficient/apptservice/ApptServiceApplication.java
@@ -2,8 +2,10 @@ package com.perficient.apptservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApptServiceApplication {
 
 	public static void main(String[] args) {
